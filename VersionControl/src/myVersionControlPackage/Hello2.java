96bd764936303c63f4e4cf8dbfb53a2e92c57dd4
jsmit37 <jsmit37@TOOLS03.dcs.bbk.ac.uk>
2014-01-20 19:17:53 +0000

    Second commit

---
+++ VersionControl/src/myVersionControlPackage/Hello2.java
@@ -0,0 +1,10 @@
+package myVersionControlPackage;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World2");
+
+	}
+
+}
---
+++ VersionControl/src/myVersionControlPackage/Hello3.java
@@ -0,0 +1,10 @@
+package myVersionControlPackage;
+
+public class Hello3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World3");
+
+	}
+
+}
