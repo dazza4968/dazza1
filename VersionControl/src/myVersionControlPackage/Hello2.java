467e732ec964650ffb554ff2626086fba7e2645f
jsmit37 <jsmit37@TOOLS03.dcs.bbk.ac.uk>
2014-01-20 19:19:49 +0000

    Change

--- VersionControl/src/myVersionControlPackage/Hello2.java
+++ VersionControl/src/myVersionControlPackage/Hello2.java
@@ -3,7 +3,7 @@ package myVersionControlPackage;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World2");
+		System.out.println("Hello World22");
 
 	}
 
